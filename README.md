91c0cdf98d52549eaf7cd7a44b9fcae34ce452d0
beomseok.choi <cbs412@naver.com>
2022-02-13 09:40:19 +0900

    Adding challenge project link

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ My portfolio website
 - [ ] 홈화면 오디오 추가
 - [ ] 프로젝트 프리뷰 비디오로 교체
 - [ ] Favicon 추가
+- [ ] 프로젝트 모달 창 레포지토리 항목
+  
+## repository 정리
--- src/Routes/About.tsx
+++ src/Routes/About.tsx
@@ -125,7 +125,7 @@ function About() {
             }}
           ></ProfileImg>
           <Bio className="profile_bio">
-            <h1>Hello, I am Beomsoeok!</h1>
+            <h1>Hello, I am Beomseok!</h1>
             <p>Thank you for your visit to my portfolio website!</p>
             <p>I have been studied development for 1 year.</p>
             <p>So I still have a lot of things to learn,</p>
